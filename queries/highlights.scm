160c6035bfae2b5448fcef1bb0c51113aa444efb
ivanjermakov <ivanjermakov1@gmail.com>
2024-02-05 03:57:32 +0100

    Highlight queries

---
+++ queries/highlights.scm
@@ -0,0 +1,70 @@
+((identifier (NAME) @type) 
+ (#match? @type "^[A-Z]"))
+(typeDef (NAME) @type)
+(traitDef (NAME) @type) 
+
+(param (pattern (NAME) @variable.parameter))
+
+((param (pattern (NAME) @variable.builtin) 
+ (#match? @variable.builtin "self")))
+((identifier (NAME) @variable.builtin) 
+ (#match? @variable.builtin "self"))
+
+(fnDef (NAME) @function)
+
+(fieldDef (NAME) @property)
+
+(addOp) @operator
+(subOp) @operator
+(multOp) @operator
+(divOp) @operator
+(expOp) @operator
+(modOp) @operator
+(accessOp) @operator
+(eqOp) @operator
+(neOp) @operator
+(geOp) @operator
+(leOp) @operator
+(gtOp) @operator
+(ltOp) @operator
+(andOp) @operator
+(orOp) @operator
+(assignOp) @operator
+(notOp) @operator
+(spreadOp) @operator
+
+(INT) @number
+(FLOAT) @number
+(STRING) @string
+(CHAR) @string
+
+(USE_KEYWORD) @keyword
+(TYPE_KEYWORD) @keyword
+(TRAIT_KEYWORD) @keyword
+(IF_KEYWORD) @keyword
+(ELSE_KEYWORD) @keyword
+(RETURN_KEYWORD) @keyword
+(BREAK_KEYWORD) @keyword
+(IMPL_KEYWORD) @keyword
+(LET_KEYWORD) @keyword
+(FN_KEYWORD) @keyword
+(WHILE_KEYWORD) @keyword
+(FOR_KEYWORD) @keyword
+(IN_KEYWORD) @keyword
+(MATCH_KEYWORD) @keyword
+
+(O_PAREN) @punctuation.delimiter
+(C_PAREN) @punctuation.delimiter
+(O_BRACKET) @punctuation.delimiter
+(C_BRACKET) @punctuation.delimiter
+(O_BRACE) @punctuation.delimiter
+(C_BRACE) @punctuation.delimiter
+
+(identifier (COLON) @punctuation.delimiter)
+
+(typeArgs
+  (O_ANGLE) @punctuation.bracket
+  (C_ANGLE) @punctuation.bracket)
+
+
+(COMMENT) @comment
